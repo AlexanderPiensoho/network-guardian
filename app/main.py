ddef542d153ecf5ab0e4e434d4b4d2a753e2e8f8
piensoho@gmail.com <piensoho@gmail.com>
2025-12-19 15:14:17 +0100

    put everything in an while loop

--- app/main.py
+++ app/main.py
@@ -2,22 +2,23 @@ import requests
 import yaml 
 from services import ping, req
 
+
 def main():
     with open ("targets.yml", "r") as file:
         targets = yaml.safe_load(file)
+        
+    while True:
+        for target in targets["targets"]:
+            name = target["name"]
+            target_type = target["type"]
 
-    for target in targets["targets"]:
-        name = target["name"]
-        target_type = target["type"]
 
-        print(f"kontroll: {name}")
-
-        if target_type == "http":
-            status = req(target["url"])
-            print(f"status: {status}")
-        elif target_type == "ping":
-            ping_host = ping(target["host"])
-            print(f"Ping: {ping_host}")
+            if target_type == "http":
+                status = req(target["url"])
+                print(f"status: {status}")
+            elif target_type == "ping":
+                ping_host = ping(target["host"])
+                print(f"Ping: {ping_host}")
 
 if __name__ == "__main__":
     main()
